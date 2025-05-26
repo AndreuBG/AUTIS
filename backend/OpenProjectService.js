ebea0e2b1b4145c71bbeb3e9a4069154ab6b2259
AndreuBG <andreueljust@gmail.com>
2025-05-26 10:00:43 +0200

    TabButton de vuelta y control de error simple

--- backend/OpenProjectService.js
+++ backend/OpenProjectService.js
@@ -11,47 +11,52 @@ static setToken(token) {
 }
 
 static async getAllProjects() {
-  const data = await fetch(`${this.API_URL}/projects`, {
-                headers: {
+    const projects = [];
+
+    try {
+        const data = await fetch(`${this.API_URL}/projects`, {
+            headers: {
                 'Authorization': 'Basic ' + btoa(`apikey:${this.API_TOKEN}`)
-                }
-            })
+            }
+        })
             .then(response => response.json())
             .then (data => data._embedded.elements)
-            .catch(function (error) {
-                console.log("Hubo un problema con los proyectos:" + error.message);
-            });
+
+            for (let i = 0; i < data.length; i++) {
+                const project = new Project(data[i].active, data[i].id, data[i].name, data[i].description.raw);
+                projects.push(project)
+            }
 
 
+    } catch (error) {
+        console.log("Hubo un problema con los proyectos:" + error.message);
+        return error;
+    }
 
-  const projects = [];
-
-  for (let i = 0; i < data.length; i++) {
-      const project = new Project(data[i].active, data[i].id, data[i].name, data[i].description.raw);
-      projects.push(project)
-  }
   return projects;
 }
     
  static async getAllTasks() {
-    const data = await fetch(`${this.API_URL}/work_packages`, {
-                headers: {
-                'Authorization': 'Basic ' + btoa(`apikey:${this.API_TOKEN}`)
-                }
-            })
-            .then(response => response.json())
-            .then(data => data._embedded.elements)
-            .catch(function (error) {
-                console.log("Hubo un problema con las tareas:" + error.message);
-            });
-
     const tasks = [];
 
-    for (let i = 0; i < data.length; i++) {
-    const task = new Task(data[i].id, data[i].subject, data[i].description.raw, data[i].startDate, data[i].dueDate, data[i]._links.project.title);
-    tasks.push(task)
-    }
+    try {
+        const data = await fetch(`${this.API_URL}/work_packages`, {
+            headers: {
+                'Authorization': 'Basic ' + btoa(`apikey:${this.API_TOKEN}`)
+            }
+        })
+            .then(response => response.json())
+            .then(data => data._embedded.elements)
 
+            for (let i = 0; i < data.length; i++) {
+                const task = new Task(data[i].id, data[i].subject, data[i].description.raw, data[i].startDate, data[i].dueDate, data[i]._links.project.title);
+                tasks.push(task)
+            }
+
+
+    } catch (error) {
+        console.error("Hubo un problema con las tareas:" + error.message);
+    }
 
     return tasks;
 }
--- backend/public/generate.js
+++ backend/public/generate.js
@@ -1,45 +1,52 @@
-//import { OpenProjectService } from "../OpenProjectService.js";
-import '../components/ProjectCard.js';
-import '../components/TaskCard.js';
-import '../components/TabButton.js';
+import './components/ProjectCard.js';
+import './components/TaskCard.js';
+import './components/TabButton.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
     try {
         const responseProject = await fetch('/getProjects');
-        const proyectos = await responseProject.json();
+
+            const proyectos = await responseProject.json();
+
+            console.log(proyectos);
 
 
-        const listaProyectos = document.getElementById('proyectos');
-        proyectos.forEach(p => {
-            const projectElement = document.createElement('project-card');
-            projectElement.setAttribute('id', p.id);
-            projectElement.setAttribute('active', p.active  );
-            projectElement.setAttribute('name', p.name);
-            projectElement.setAttribute('description', p.description);
+            const listaProyectos = document.getElementById('proyectos');
+            proyectos.forEach(p => {
+                const projectElement = document.createElement('project-card');
+                projectElement.setAttribute('id', p.id);
+                projectElement.setAttribute('active', p.active  );
+                projectElement.setAttribute('name', p.name);
+                projectElement.setAttribute('description', p.description);
 
-            listaProyectos.appendChild(projectElement);
-        });
+                listaProyectos.appendChild(projectElement);
+            });
+
+
+
 
         const responseTask = await fetch('/getTasks');
-        const tareas = await responseTask.json();
 
-        const listaTareas = document.getElementById('tareas');
-                tareas.forEach(t => {
-                    const taskElement = document.createElement('task-card');
-                    taskElement.setAttribute('id', t.id);
-                    taskElement.setAttribute('subject', t.subject);
-                    taskElement.setAttribute('description', t.description);
-                    taskElement.setAttribute('startDate', t.startDate);
-                    taskElement.setAttribute('dueDate', t.dueDate);
-                    taskElement.setAttribute('project', t.project);
+        if (!responseTask.ok) {
+            console.error("Error a la hora de conseguir las tareas")
+        } else {
+            const tareas = await responseTask.json();
 
-                    listaTareas.appendChild(taskElement);
+            const listaTareas = document.getElementById('tareas');
+            tareas.forEach(t => {
+                const taskElement = document.createElement('task-card');
+                taskElement.setAttribute('id', t.id);
+                taskElement.setAttribute('subject', t.subject);
+                taskElement.setAttribute('description', t.description);
+                taskElement.setAttribute('startDate', t.startDate);
+                taskElement.setAttribute('dueDate', t.dueDate);
+                taskElement.setAttribute('project', t.project);
 
-                });
-        
+                listaTareas.appendChild(taskElement);
 
+            });
+        }
 
-            
     } catch(error) {
         console.error(error)
     }
